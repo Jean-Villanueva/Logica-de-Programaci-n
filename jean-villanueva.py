baf2cf06df87052b554f805be0e04bbf677dfc00
Jean-Villanueva <jeancarlovl10@gmail.com>
2024-10-06 16:20:59 -0600

    jean-villanueva.py

--- jean-villanueva.py
+++ jean-villanueva.py
@@ -1,2 +1,11 @@
-MY_CONSTANT = "Mi Constante" 
+MY_CONSTANT = "Mi Constante" #en python no hay constante, se escribe asi para que el que este viendo el codigo sepa que esta es una supuesta constante
 MY_CONSTANT= "PLAZA"
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("Hola pyton")
